b8b32f7d973b28cfbe0415c29f8dc8f2512c14bc
Phuong Nguyen <pnguyen9968@gmail.com>
2023-09-17 18:47:39 +0700

    chore: [Ticket-12] basics-assignment-1

---
+++ basics-assignment-1-finish-nlp/src/app/success-message/success-message.component.ts
@@ -0,0 +1,9 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector: 'success-message',
+    templateUrl: './success-message.component.html',
+})
+export class SuccessMessageComponent {
+
+}
---
+++ basics-assignment-1-finish-nlp/src/app/warning-message/warning-message.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-warning-message',
+  templateUrl: './warning-message.component.html',
+  styleUrls: ['./warning-message.component.css']
+})
+export class WarningMessageComponent {
+
+}
