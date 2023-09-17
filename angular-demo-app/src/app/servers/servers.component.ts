89066ec23eb90a4b3e398d61a528bd81e1fe3117
Phuong Nguyen <pnguyen9968@gmail.com>
2023-09-17 21:17:18 +0700

    chore: [Ticket-13] Passing and Using Data with Event Binding

--- angular-demo-app/src/app/servers/servers.component.ts
+++ angular-demo-app/src/app/servers/servers.component.ts
@@ -1,27 +1,30 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  // selector: '[app-servers]',
-  // selector: '.app-servers',
-  selector: 'app-servers',
-  // template: `<app-server></app-server>`,
-  templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+    // selector: '[app-servers]',
+    // selector: '.app-servers',
+    selector: 'app-servers',
+    // template: `<app-server></app-server>`,
+    templateUrl: './servers.component.html',
+    styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
 
-  allowNewServer = false;
-  serverCreationStatus = 'No server was created!';
+    allowNewServer = false;
+    serverCreationStatus = 'No server was created!';
+    serverName = '';
 
-  constructor() {
-    setTimeout(() => {
-      this.allowNewServer = true;
-    }, 2000);
-  }
+    constructor() {
+        setTimeout(() => {
+            this.allowNewServer = true;
+        }, 2000);
+    }
 
-  onCreateServer() {
-    this.serverCreationStatus = 'Server was created!';
-  }
+    onCreateServer() {
+        this.serverCreationStatus = 'Server was created!';
+    }
 
-
+    onUpdateServerName(event: Event) {
+        this.serverName = (<HTMLInputElement>event.target).value;
+    }
 }
