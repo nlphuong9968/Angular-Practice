3d07e0c23c5db4efa0288b9fa29e565eded97ad1
Phuong Nguyen <pnguyen9968@gmail.com>
2023-09-19 00:23:13 +0700

    chore: [Ticket-17] basics-assignment-2-finish

--- basics-assignment-2-start/src/app/app.module.ts
+++ basics-assignment-2-start/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { UsernameComponent } from './username/username.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, UsernameComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
---
+++ basics-assignment-2-start/src/app/username/username.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'app-username',
+    templateUrl: './username.component.html',
+    styleUrls: ['./username.component.css']
+})
+export class UsernameComponent {
+    userName: string = '';
+
+    resetUsername() {
+        this.userName = '';
+    }
+}
