761870e921a73cf70a47438cf201dab9df3959a2
Phuong Nguyen <pnguyen9968@gmail.com>
2023-09-19 17:56:03 +0700

    chore: [Ticket-20] Styling Elements Dynamically with ngStyle

--- angular-demo-app/src/app/server/server.component.ts
+++ angular-demo-app/src/app/server/server.component.ts
@@ -4,11 +4,19 @@ import {Component} from "@angular/core";
     selector: 'app-server',
     templateUrl: './server.component.html'
 })
-export class ServerComponent{
+export class ServerComponent {
     serverID: number = 10;
     serverStatus: string = "offline";
 
+    constructor() {
+        this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+    }
+
     getServerStatus() {
         return this.serverStatus;
+    }
+
+    getColor() {
+        return this.serverStatus === 'online' ? 'red' : 'green';
     }
 }
