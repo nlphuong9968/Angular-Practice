534d5c250fc047d2d617e206addcb4be0bcc3d05
Phuong Nguyen <pnguyen9968@gmail.com>
2023-09-19 21:30:53 +0700

    feat: [Project-01] Setting up the Application

--- angular-recipe-book-app/src/app/app.component.spec.ts
+++ angular-recipe-book-app/src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-demo-app'`, () => {
+  it(`should have as title 'angular-recipe-book-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-demo-app');
+    expect(app.title).toEqual('angular-recipe-book-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-demo-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-recipe-book-app app is running!');
   });
 });
