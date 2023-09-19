10cb8a552cd2dd4d66d12e83f34402b38718ce67
Phuong Nguyen <pnguyen9968@gmail.com>
2023-09-19 20:49:30 +0700

    chore: [Ticket-24] basics-assignment-3-finish

--- basics-assignment-3-start/src/app/app.component.ts
+++ basics-assignment-3-start/src/app/app.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+    showParagraph = false;
+    logs = [];
+
+    onShowParagraph() {
+        this.showParagraph = !this.showParagraph;
+        this.logs.push(new Date());
+    }
 }
