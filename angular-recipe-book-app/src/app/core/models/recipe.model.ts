991dad823b53ba74f911af61afde1ae70fb7d9e0
Phuong Nguyen <pnguyen9968@gmail.com>
2023-09-20 21:59:42 +0700

    chore: [Project-06] Creating a "Recipe" Model

--- angular-recipe-book-app/src/app/core/models/recipe.model.ts
+++ angular-recipe-book-app/src/app/core/models/recipe.model.ts
@@ -1,2 +1,11 @@
-export class RecipeModel {
+export class Recipe {
+    public name: string;
+    public description: string;
+    public imagePath: string;
+
+    constructor(name: string, desc: string, imagePath: string) {
+        this.name = name;
+        this.description = desc;
+        this.imagePath = imagePath;
+    }
 }
