dd4411e26a68fb264a814a8d7038402db2fde98e
Phuong Nguyen <pnguyen9968@gmail.com>
2023-09-20 21:06:40 +0700

    chore: [Project-02] Creating the Components

--- angular-recipe-book-app/src/app/app.module.ts
+++ angular-recipe-book-app/src/app/app.module.ts
@@ -1,16 +1,30 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+
+import {HeaderComponent} from './core/layout/header/header.component';
+import {RecipeListComponent} from "./feature/recipes/recipe-list/recipe-list.component";
+import {RecipeDetailComponent} from "./feature/recipes/recipe-detail/recipe-detail.component";
+import {RecipesComponent} from './feature/recipes/recipes.component';
+import {ShoppingEditComponent} from "./feature/shopping-list/shopping-edit/shopping-edit.component";
+import {ShoppingListComponent} from "./feature/shopping-list/shopping-list.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ShoppingListComponent,
+        ShoppingEditComponent,
+        RecipeListComponent,
+        RecipeDetailComponent,
+        HeaderComponent,
+        RecipesComponent
+    ],
+    imports: [
+        BrowserModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
