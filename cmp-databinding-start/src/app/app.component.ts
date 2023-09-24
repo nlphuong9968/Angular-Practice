6afe64a86f82a1c81a860a1ab715272a8a6ed804
Phuong Nguyen <pnguyen9968@gmail.com>
2023-09-24 21:09:15 +0700

    chore: [Ticket-36] Seeing Lifecycle Hooks in Action

--- cmp-databinding-start/src/app/app.component.ts
+++ cmp-databinding-start/src/app/app.component.ts
@@ -24,5 +24,11 @@ export class AppComponent {
         });
     }
 
-    protected readonly event = event;
+    onChangeFirst() {
+        this.serverElements[0].name = 'Changed!'
+    }
+
+    onDestroyFirst() {
+        this.serverElements.splice(0, 1);
+    }
 }
