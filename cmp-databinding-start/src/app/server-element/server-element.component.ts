bf96b67d6cbc55cc02691aab7446e77a25cfe10f
Phuong Nguyen <pnguyen9968@gmail.com>
2023-09-24 18:03:57 +0700

    chore: [Ticket-28] Binding to Custom Properties

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-server-element',
-  templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+    selector: 'app-server-element',
+    templateUrl: './server-element.component.html',
+    styleUrls: ['./server-element.component.css']
 })
 export class ServerElementComponent implements OnInit {
+    @Input() element: { name: string, type: string, content: string };
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
 }
