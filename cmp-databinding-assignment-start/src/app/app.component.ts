817b50a214aa41eafad6ff62d6dde83b17b3ad37
Phuong Nguyen <pnguyen9968@gmail.com>
2023-09-27 00:12:01 +0700

    chore: [Ticket-40] cmp-databinding-assignment-finish

--- cmp-databinding-assignment-start/src/app/app.component.ts
+++ cmp-databinding-assignment-start/src/app/app.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+    evenNumbers: number[] = [];
+    oddNumbers: number[] = [];
+
+    onIntervalFired(eventNumber: number) {
+        eventNumber % 2 === 0 ? this.evenNumbers.push(eventNumber) : this.oddNumbers.push(eventNumber);
+    }
 }
