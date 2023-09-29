5d603fade1dc0889839bf0e043c7b33564fe08ac
Phuong Nguyen <pnguyen9968@gmail.com>
2023-09-29 21:55:05 +0700

    chore: [Project-16] Passing Data with Event and Property Binding (Combined)

--- angular-recipe-book-app/src/app/feature/recipes/recipe-list/recipe-list.component.ts
+++ angular-recipe-book-app/src/app/feature/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 import {Recipe} from "../../../core/models/recipe.model";
 
 @Component({
@@ -7,13 +7,19 @@ import {Recipe} from "../../../core/models/recipe.model";
     styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+    @Output() recipeWasSelected: EventEmitter<Recipe> = new EventEmitter<Recipe>();
+
     recipes: Recipe[] = [
         new Recipe('A Test Recipe', 'This is a simply test',
             'https://www.foodandwine.com/thmb/Hb86ayDLFHpGOuvbziM5Z5c4XNc=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Gnocchi-Parisienne-FT-RECIPE0523-a2ca65b48ffc4569adf8baafd009dfaa.jpg'),
-        new Recipe('A Test Recipe', 'This is a simply test',
+        new Recipe('Another Test Recipe', 'This is a simply test',
             'https://www.foodandwine.com/thmb/Hb86ayDLFHpGOuvbziM5Z5c4XNc=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Gnocchi-Parisienne-FT-RECIPE0523-a2ca65b48ffc4569adf8baafd009dfaa.jpg'),
     ];
 
     constructor() {
+    }
+
+    onRecipeSelected(recipeEl: Recipe) {
+        this.recipeWasSelected.emit(recipeEl);
     }
 }
--- angular-recipe-book-app/src/app/feature/recipes/recipes.component.ts
+++ angular-recipe-book-app/src/app/feature/recipes/recipes.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Recipe} from "../../core/models/recipe.model";
 
 @Component({
-  selector: 'app-recipes',
-  templateUrl: './recipes.component.html',
-  styleUrls: ['./recipes.component.css']
+    selector: 'app-recipes',
+    templateUrl: './recipes.component.html',
+    styleUrls: ['./recipes.component.css']
 })
 export class RecipesComponent {
+
+    selectedRecipe: Recipe;
 
 }
