0bb7851b5613cea253b5fe839a3557e0d69bb023
Phuong Nguyen <pnguyen9968@gmail.com>
2023-10-08 22:06:31 +0700

    chore: [Ticket-59] services-assignment

---
+++ services-assignment-start/src/app/active-users/active-users.component.ts
@@ -0,0 +1,18 @@
+import {Component, Input} from '@angular/core';
+import {UserService} from "../services/user.service";
+
+@Component({
+    selector: 'app-active-users',
+    templateUrl: './active-users.component.html',
+    styleUrls: ['./active-users.component.css']
+})
+export class ActiveUsersComponent {
+    @Input() users: string[];
+
+    constructor(private userService: UserService) {
+    }
+
+    onSetToInactive(id: number) {
+        this.userService.setToInactive(id);
+    }
+}
