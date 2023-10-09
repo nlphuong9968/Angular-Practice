b1450fb06b072da7c6cb44f3a1b7da1627760ce5
Phuong Nguyen <pnguyen9968@gmail.com>
2023-10-09 22:38:34 +0700

    chore: [Project-21] Managing Recipes in a Recipe Service

--- angular-recipe-book-app/src/app/core/services/recipe.service.ts
+++ angular-recipe-book-app/src/app/core/services/recipe.service.ts
@@ -1,9 +1,14 @@
-import { Injectable } from '@angular/core';
+import {Recipe} from "../models/recipe.model";
 
-@Injectable({
-  providedIn: 'root'
-})
 export class RecipeService {
+    recipes: Recipe[] = [
+        new Recipe('A Test Recipe', 'This is a simply test',
+            'https://www.foodandwine.com/thmb/Hb86ayDLFHpGOuvbziM5Z5c4XNc=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Gnocchi-Parisienne-FT-RECIPE0523-a2ca65b48ffc4569adf8baafd009dfaa.jpg'),
+        new Recipe('Another Test Recipe', 'This is a simply test',
+            'https://www.foodandwine.com/thmb/Hb86ayDLFHpGOuvbziM5Z5c4XNc=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Gnocchi-Parisienne-FT-RECIPE0523-a2ca65b48ffc4569adf8baafd009dfaa.jpg'),
+    ];
 
-  constructor() { }
+    getRecipe() {
+        return this.recipes.slice();
+    }
 }
--- angular-recipe-book-app/src/app/feature/recipes/recipe-list/recipe-list.component.ts
+++ angular-recipe-book-app/src/app/feature/recipes/recipe-list/recipe-list.component.ts
@@ -1,22 +1,23 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Recipe} from "../../../core/models/recipe.model";
+import {RecipeService} from "../../../core/services/recipe.service";
 
 @Component({
     selector: 'app-recipe-list',
     templateUrl: './recipe-list.component.html',
     styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+
     @Output() recipeWasSelected: EventEmitter<Recipe> = new EventEmitter<Recipe>();
 
-    recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is a simply test',
-            'https://www.foodandwine.com/thmb/Hb86ayDLFHpGOuvbziM5Z5c4XNc=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Gnocchi-Parisienne-FT-RECIPE0523-a2ca65b48ffc4569adf8baafd009dfaa.jpg'),
-        new Recipe('Another Test Recipe', 'This is a simply test',
-            'https://www.foodandwine.com/thmb/Hb86ayDLFHpGOuvbziM5Z5c4XNc=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Gnocchi-Parisienne-FT-RECIPE0523-a2ca65b48ffc4569adf8baafd009dfaa.jpg'),
-    ];
+    recipes: Recipe[];
 
-    constructor() {
+    constructor(private recipeService: RecipeService) {
+    }
+
+    ngOnInit(): void {
+        this.recipes = this.recipeService.getRecipe();
     }
 
     onRecipeSelected(recipeEl: Recipe) {
