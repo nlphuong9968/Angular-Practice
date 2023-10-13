7448a8039aa05fcf5b0195d7088c80edf72242e3
Phuong Nguyen <pnguyen9968@gmail.com>
2023-10-13 22:31:30 +0700

    chore: [Ticket-80] Using a Fake Auth Service

--- routing-start/src/app/home/home.component.ts
+++ routing-start/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
+import {AuthService} from "../auth.service";
 
 @Component({
     selector: 'app-home',
@@ -8,7 +9,7 @@ import {Router} from "@angular/router";
 })
 export class HomeComponent implements OnInit {
 
-    constructor(private router: Router) {
+    constructor(private router: Router, private authService: AuthService) {
     }
 
     ngOnInit() {
@@ -16,5 +17,13 @@ export class HomeComponent implements OnInit {
 
     onLoandingServers() {
         this.router.navigate(['servers', 5, 'edit'], {queryParams: {allowEdit: '1'}, fragment: 'loading'});
+    }
+
+    onLogout() {
+        this.authService.logout();
+    }
+
+    onLogin() {
+        this.authService.login();
     }
 }
