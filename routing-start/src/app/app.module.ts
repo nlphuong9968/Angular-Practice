bcbcc3830564d82ef70949f13fb0d231df0fd1ae
Phuong Nguyen <pnguyen9968@gmail.com>
2023-10-13 21:26:50 +0700

    chore: [Ticket-76] Redirecting and Wildcard Routes

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -12,6 +12,7 @@ import {EditServerComponent} from './servers/edit-server/edit-server.component';
 import {ServerComponent} from './servers/server/server.component';
 import {ServersService} from './servers/servers.service';
 import {RouterModule, Routes} from "@angular/router";
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 
 const appRoute: Routes = [
     {path: '', component: HomeComponent},
@@ -26,7 +27,12 @@ const appRoute: Routes = [
             {path: ':id/edit', component: EditServerComponent},
         ]
     },
-
+    {
+        path: 'not-found', component: PageNotFoundComponent
+    },
+    {
+        path: '**', redirectTo: '/not-found'
+    }
 ]
 
 @NgModule({
@@ -37,7 +43,8 @@ const appRoute: Routes = [
         ServersComponent,
         UserComponent,
         EditServerComponent,
-        ServerComponent
+        ServerComponent,
+        PageNotFoundComponent
     ],
     imports: [
         BrowserModule,
